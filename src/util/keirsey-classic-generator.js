7c809caf99bb549e1fbd96bf26797881e972eb2e
nunya biz <nunya@bizniz.con>
2020-10-16 00:44:00 -0400

    Consolidate generator into one function

--- src/util/keirsey-classic-generator.js
+++ src/util/keirsey-classic-generator.js
@@ -1,9 +1,7 @@
 // Utility functions for generating boilerplate JSON for building out assessments
-import { questionText } from '../res/keirsey-classic-questions';
 
-// Array
 // Generate tedious boilerplate JSON for Classic Keirsey assessment
-export const generateKeirseyClassicJSON = () => {
+export const generateKeirseyClassicBoilerplate = () => {
     const questions = [];
     const typeSequence = ["E", "I", "S", "N", "S", "N", "T", "F", "T", "F", "J", "P", "J", "P"];
     let type = 0;
@@ -18,31 +16,15 @@ export const generateKeirseyClassicJSON = () => {
 
         q.answers[0] = {};
         q.answers[0].id = "a";
+        q.answers[0].text = "CHANGE_ME";
         q.answers[0].type = typeSequence[type];
         type === 13 ? type = 0 : type += 1;
 
         q.answers[1] = {};
         q.answers[1].id = "b";
+        q.answers[1].text = "CHANGE_ME";
         q.answers[1].type = typeSequence[type];
         type === 13 ? type = 0 : type += 1;
-
-        questions.push(q);
-    }
-
-    return questions;
-}
-
-// Array
-// Generate tedious boilerplate JSON for Classic Keirsey assessment questions
-export const generateKeirseyClassicQuestionsBoilerplate = () => {
-    const questions = [];
-
-    for (let i = 0; i < 70; i++) {
-        const q = {};
-
-        // Question id begins at 1 to avoid confusion between index and question number
-        q.id = i + 1;
-        q.text = "CHANGE_ME"
 
         questions.push(q);
     }

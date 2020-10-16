8789197389c009cff0f5cfe0fe7f55c9f37179a1
nunya biz <nunya@bizniz.con>
2020-10-15 22:51:53 -0400

    Make `questions` JSON usable as module in JS files

---
+++ src/res/keirsey-classic-questions.js
@@ -0,0 +1,282 @@
+export const questionText = [
+    {
+        "id": 1,
+        "text": "At a party, do you"
+    },
+    {
+        "id": 2,
+        "text": "Are you more"
+    },
+    {
+        "id": 3,
+        "text": "Is it worse to"
+    },
+    {
+        "id": 4,
+        "text": "Are you more impressed by"
+    },
+    {
+        "id": 5,
+        "text": "Are you more drawn toward the"
+    },
+    {
+        "id": 6,
+        "text": "Do you prefer to work"
+    },
+    {
+        "id": 7,
+        "text": "Do you tend to choose"
+    },
+    {
+        "id": 8,
+        "text": "At parties, do you"
+    },
+    {
+        "id": 9,
+        "text": "Are you more attracted to"
+    },
+    {
+        "id": 10,
+        "text": "Are you more interested in"
+    },
+    {
+        "id": 11,
+        "text": "In judging others, are you more swayed by"
+    },
+    {
+        "id": 12,
+        "text": "In approaching others is your inclination to be somewhat"
+    },
+    {
+        "id": 13,
+        "text": "Are you more"
+    },
+    {
+        "id": 14,
+        "text": "Does it bother you more having things"
+    },
+    {
+        "id": 15,
+        "text": "In your social groups, do you"
+    },
+    {
+        "id": 16,
+        "text": "In doing ordinary things, are you more likely to"
+    },
+    {
+        "id": 17,
+        "text": "Writers should"
+    },
+    {
+        "id": 18,
+        "text": "Which appeals to you more:"
+    },
+    {
+        "id": 19,
+        "text": "Are you more comfortable in making"
+    },
+    {
+        "id": 20,
+        "text": "Do you want things"
+    },
+    {
+        "id": 21,
+        "text": "Would you say you are more"
+    },
+    {
+        "id": 22,
+        "text": "In phoning, do you"
+    },
+    {
+        "id": 23,
+        "text": "Facts"
+    },
+    {
+        "id": 24,
+        "text": "Are visionaries"
+    },
+    {
+        "id": 25,
+        "text": "Are you more often"
+    },
+    {
+        "id": 26,
+        "text": "Is it worse to be"
+    },
+    {
+        "id": 27,
+        "text": "Should one usually let events occur"
+    },
+    {
+        "id": 28,
+        "text": "Do you feel better about"
+    },
+    {
+        "id": 29,
+        "text": "In company do you"
+    },
+    {
+        "id": 30,
+        "text": "Common sense is"
+    },
+    {
+        "id": 31,
+        "text": "Children often do not"
+    },
+    {
+        "id": 32,
+        "text": "In making decisions, do you feel more comfortable with"
+    },
+    {
+        "id": 33,
+        "text": "Are you more"
+    },
+    {
+        "id": 34,
+        "text": "Which is more admirable:"
+    },
+    {
+        "id": 35,
+        "text": "Do you put more value on the"
+    },
+    {
+        "id": 36,
+        "text": "Does new and non-routine interaction with others"
+    },
+    {
+        "id": 37,
+        "text": "Are you more frequently"
+    },
+    {
+        "id": 38,
+        "text": "Are you more likely to"
+    },
+    {
+        "id": 39,
+        "text": "Which is more satisfying:"
+    },
+    {
+        "id": 40,
+        "text": "Which rules you more:"
+    },
+    {
+        "id": 41,
+        "text": "Are you more comfortable with work that is"
+    },
+    {
+        "id": 42,
+        "text": "Do you tend to look for"
+    },
+    {
+        "id": 43,
+        "text": "Do you prefer"
+    },
+    {
+        "id": 44,
+        "text": "Do you go more by"
+    },
+    {
+        "id": 45,
+        "text": "Are you more interested in"
+    },
+    {
+        "id": 46,
+        "text": "Which is more of a compliment:"
+    },
+    {
+        "id": 47,
+        "text": "Do you value in yourself more that you are"
+    },
+    {
+        "id": 48,
+        "text": "Do you more often prefer the"
+    },
+    {
+        "id": 49,
+        "text": "Are you more comfortable"
+    },
+    {
+        "id": 50,
+        "text": "Do you"
+    },
+    {
+        "id": 51,
+        "text": "Are you more likely to trust your"
+    },
+    {
+        "id": 52,
+        "text": "Do you feel"
+    },
+    {
+        "id": 53,
+        "text": "Which person is more to be complimented: one of"
+    },
+    {
+        "id": 54,
+        "text": "Are you inclined more to be"
+    },
+    {
+        "id": 55,
+        "text": "Is it preferable mostly to"
+    },
+    {
+        "id": 56,
+        "text": "In relationships, should most things be"
+    },
+    {
+        "id": 57,
+        "text": "When the phone rings, do you"
+    },
+    {
+        "id": 58,
+        "text": "Do you prize more in yourself"
+    },
+    {
+        "id": 59,
+        "text": "Are you drawn more to"
+    },
+    {
+        "id": 60,
+        "text": "Which seems the greater error:"
+    },
+    {
+        "id": 61,
+        "text": "Do you see yourself as basically"
+    },
+    {
+        "id": 62,
+        "text": "Which situation appeals to you more:"
+    },
+    {
+        "id": 63,
+        "text": "Are you a person that is more"
+    },
+    {
+        "id": 64,
+        "text": "Are you more inclined to be"
+    },
+    {
+        "id": 65,
+        "text": "In writings, do you prefer"
+    },
+    {
+        "id": 66,
+        "text": "Is it harder for you to"
+    },
+    {
+        "id": 67,
+        "text": "Which do you wish more for yourself:"
+    },
+    {
+        "id": 68,
+        "text": "Which is the greater fault:"
+    },
+    {
+        "id": 69,
+        "text": "Do you prefer the"
+    },
+    {
+        "id": 70,
+        "text": "Do you tend to be more"
+    }
+]
